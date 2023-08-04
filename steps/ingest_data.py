508e2a405f063cbb336b3b1c9d792fdb4225ebc1
Nikhil <NikhilDevassia>
2023-08-04 12:41:09 +0530

    pipeline, steps created

---
+++ steps/ingest_data.py
@@ -0,0 +1,24 @@
+import logging #Capturing information, messages, and events
+import pandas as pd
+from zenml import step
+
+class IngestData:
+    """
+    Ingesting the data from the data path
+    """
+    def __init__(self, data_path:str):
+        self.data_path = data_path
+
+    def get_data(self):
+        logging.info(f"Ingesting data from {self.data_path}")
+        return pd.read_csv(self.data_path)
+
+@step
+def ingest_df(data_path:str) -> pd.DataFrame:
+    try:
+        ingest_data = IngestData(data_path)
+        df = ingest_data.get_data()
+        return df 
+    except Exception as e:
+        logging.error(f"Error while ingesting data: {e}")
+        raise e 
